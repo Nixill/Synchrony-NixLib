0a07f5a0d49c2a08da99306f86d3c643a59816bf
Steven <github@steven-h.net>
2022-06-12 02:48:00 -0400

    Begin internationalization!

---
+++ scripts/i18n/KeyBank.lua
@@ -0,0 +1,11 @@
+local module = {}
+
+module.No       = L "No"
+module.Yes      = L "Yes"
+module.Done     = L "Done"
+module.Cancel   = L "Cancel"
+module.Reset    = L "Reset"
+module.Default  = L "Default"
+module.Continue = L "Continue"
+
+return module
